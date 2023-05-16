5160eea6c83e3e57c466677e315ade491e9731db
cinnamew <101888513+cinnamew@users.noreply.github.com>
2023-05-16 14:16:26 -0700

    i hate unity (inventory struggling)

---
+++ Assets/Scenes/2_Main/InventoryItemData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class InventoryItemData : ScriptableObject
+{
+    public string id;
+    public string displayName;
+    public Sprite icon;
+    public GameObject prefab;
+}
