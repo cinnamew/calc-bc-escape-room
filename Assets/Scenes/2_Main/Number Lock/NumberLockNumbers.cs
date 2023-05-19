b7aa35121404fca2cb88a699a3e7df96362fa2ae
cinnamew <101888513+cinnamew@users.noreply.github.com>
2023-05-19 12:03:05 -0700

    number lock basics
    just adding & subtracting to a number

---
+++ Assets/Scenes/2_Main/Number Lock/NumberLockNumbers.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberLockNumbers : MonoBehaviour
+{
+    
+    [SerializeField] TMP_Text txt;
+    [SerializeField] int currNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //text = this.GetComponent<TMPro.TextMeshProUGUI>();
+        //i can't rlly figure this out (L unity api) so i'm just gonna set it up in the inspector
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Add() {
+        currNum++;
+        if(currNum == 10) currNum = 0;
+        txt.text = currNum + "";
+    }
+
+    public void Remove() {
+        currNum--;
+        if(currNum == -1) currNum = 9;
+        txt.text = currNum + "";
+    }
+}
